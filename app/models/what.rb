aeec5746429eb561a73f06551aa8f022752f12fa
Dan <dansification@gmail.com>
2020-08-13 17:36:28 -0400

    Add cost to the circumstance

--- app/models/what.rb
+++ app/models/what.rb
@@ -1,14 +1,15 @@
 class What
-  def initialize(circumstance, duration:)
+  def initialize(circumstance, duration:, cost:)
     @circumstance = circumstance
     @duration = duration
+    @cost = cost
   end
 
-  attr_reader :circumstance
+  attr_reader :circumstance, :duration, :cost
 end
 
-def What(circumstance, duration: 0)
+def What(circumstance, duration: 0, cost: 0)
   fail ArgumentError unless circumstance
 
-  What.new(circumstance, duration: duration)
+  What.new(circumstance, duration: duration, cost: cost)
 end
