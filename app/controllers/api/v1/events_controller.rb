026c630e9c6905352bbbb4e63be7acaccd535b92
Dan <dansification@gmail.com>
2020-08-15 11:06:27 -0400

    Add Events POST endpoint

---
+++ app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class EventsController < ApplicationController
+      def create
+        render json: serializer(event)
+      end
+
+      private
+
+      def event
+        EventProcess.new(
+          event_params.to_h.symbolize_keys.merge(
+            calendar: calendar,
+            event_types: event_types
+          )
+        ).submitted
+      end
+
+      def calendar
+        Calendar.new
+      end
+
+      def event_types
+        []
+      end
+
+      def serializer(serializable)
+        EventSerializer.new(serializable).serializable_hash.to_json
+      end
+
+      def event_params
+        params.require(:event).permit!
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v1/events_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "POST /api/v1/calendars/:calendar_id/events" do
+  it "returns events" do
+    calendar = double("calendar", id: SecureRandom.uuid)
+    params = {
+      calendar_id: SecureRandom.uuid,
+      event_type_ids: [SecureRandom.uuid],
+      event: {
+        event_id: SecureRandom.uuid,
+        starts_at: Time.now,
+        invitees: [{ name: "Invitee", email: "email@email.com" }],
+      }
+    }
+
+    post "/api/v1/calendars/#{calendar.id}/events", params: params
+
+    expect(json_body.dig("data", "id")).to eq(params[:event][:event_id])
+  end
+end
