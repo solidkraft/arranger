dff974a97933c0a00573d559499590cbdd167cab
Dan <dansification@gmail.com>
2020-08-15 10:27:02 -0400

    Add Event multi-step process

---
+++ app/models/event_process.rb
@@ -0,0 +1,71 @@
+require "who"
+require "what"
+require "when"
+require "where"
+
+class EventProcess
+  def initialize(calendar:, event_id:, event_types:, starts_at:, invitees:)
+    @calendar = calendar
+    @event_id = event_id
+    @event_types = event_types
+    @starts_at = starts_at
+    @invitees = invitees
+  end
+
+  attr_accessor :state
+  attr_reader :calendar, :event_id, :event_types, :starts_at, :invitees
+
+  def submitted
+    return unless calendar.available?(starts_at)
+
+    host = calendar.account
+    Who(host: host, invitees: invitees)
+
+    self.state = :available
+
+    availability_confirmed
+  end
+
+  def availability_confirmed
+    return unless state == :available
+    return unless calendar.includes_type?(event_types)
+
+    event_types.each do |event_type|
+      circumstance = event_type.name
+      duration = event_type.duration
+      cost = event_type.cost
+      What(circumstance, duration: duration, cost: cost)
+    end
+
+    self.state = :type_selected
+
+    event_types_checked
+  end
+
+  def event_types_checked
+    return unless state == :type_selected
+
+    location = calendar.location
+
+    When(starts_at)
+    Where(location)
+
+    self.state = :scheduled
+
+    scheduled
+  end
+
+  def scheduled
+    return unless state == :scheduled
+
+    self.state = :complete
+
+    Event.new(
+      id: event_id,
+      event_types: event_types,
+      starts_at: starts_at,
+      invitees: invitees,
+      location: calendar.location
+    )
+  end
+end
---
+++ spec/models/event_process_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe EventProcess do
+  it "processes event" do
+    calendar = Calendar.new
+    event_id = SecureRandom.uuid
+    event_types = [double("event_type", name: "Type", duration: 30, cost: 40)]
+    starts_at = Time.now
+    invitees = [double("invitee")]
+    process = EventProcess.new(
+      calendar: calendar,
+      event_id: event_id,
+      event_types: event_types,
+      starts_at: starts_at,
+      invitees: invitees
+    )
+
+    result = process.submitted
+
+    expect(process.state).to eq(:complete)
+    expect(result.id).to eq(event_id)
+  end
+end
